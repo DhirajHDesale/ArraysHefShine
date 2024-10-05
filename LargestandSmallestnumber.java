3115446d9424f8b7d44b69d0eba3ff54c21178dd
Dhiraj Desale <79238778+DhirajHDesale@users.noreply.github.com>
2024-10-05 17:56:12 +0530

    Largest & Smallest number

---
+++ LargestandSmallestnumber.java
@@ -0,0 +1,26 @@
+public class MyClass 
+{
+	public static void main(String[] args) 
+	{
+		int a[] = 
+		{
+			12, 1234, 45, 67, 1 
+		}
+		;
+		int n = a.length;
+		myCode(a, n);
+	}
+	public static void myCode(int a[],int n) 
+	{
+		int max = a[0];
+		int min = a[0];
+		for(int i = 0;i<a.length;i++)
+		{
+			if(a[i]>max) 
+			max = a[i];
+			else
+			min = a[i];
+		}
+		System.out.println("Max : "+max+"\n"+"Min : "+min);
+	}
+}
